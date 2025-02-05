224480a6514799f1bdeca6ea4eb9f1b0b5d988ed
@Nestorsj <nestor@soyyo.com>
2025-02-05 19:18:13 +0100

    Creo clase Principal y aporto un syso con el error actual

---
+++ pruebaAct4/src/pruebaAct4/Principal.java
@@ -0,0 +1,11 @@
+package pruebaAct4;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hola, sigue sin funcionar la importacion de LocalDate");
+
+	}
+
+}
