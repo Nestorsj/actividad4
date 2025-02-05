8e67f027abae9e5ac56b780465a5bd80410c4454
@Nestorsj <nestor@soyyo.com>
2025-02-05 19:23:53 +0100

    Ya se ha solucionado el error al vincular con remoto y valido

--- pruebaAct4/src/pruebaAct4/Principal.java
+++ pruebaAct4/src/pruebaAct4/Principal.java
@@ -1,10 +1,16 @@
 package pruebaAct4;
 
+import java.time.LocalDate;
+
 public class Principal {
 
 	public static void main(String[] args) {
 		
 		System.out.println("Hola, sigue sin funcionar la importacion de LocalDate");
+		
+		LocalDate hoy = LocalDate.now();
+		
+		System.out.println("Hoy es: " + hoy);
 
 	}
 
