fdf3b1919c56b630229dd49d002a4dc248a221c9
@Nestorsj <nestor@soyyo.com>
2025-02-05 22:09:25 +0100

    Añado método factorial fácil

--- pruebaAct4/src/pruebaAct4/Principal.java
+++ pruebaAct4/src/pruebaAct4/Principal.java
@@ -13,6 +13,19 @@ public class Principal {
 		System.out.println("Hoy es: " + hoy);
 
 		System.out.println("Ya si que funciona todo correctamente");
+		
+		System.out.println(factorialFacil(5));
+	}
+	
+	public static int factorialFacil(int num) {
+		
+		int fact = 1;
+		
+		for(int i=2;i<=num;i++) {
+			fact = fact * i;
+		}
+		
+		return fact;
 	}
 
 }
